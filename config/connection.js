53914f378120c426ba85f8686b6f5f2927066b9d
ichoi21 <ihchoi@gmail.com>
2020-06-24 19:53:54 -0700

    update deploy files

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "F@rt0minor",
-  database: "todo_db",
-});
+let connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(proccess.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "F@rt0minor",
+    database: "todo_db",
+  });
+}
 
 connection.connect((err) => {
   if (err) throw err;
